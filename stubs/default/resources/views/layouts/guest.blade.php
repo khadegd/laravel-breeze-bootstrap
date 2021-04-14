523ca1f4aea3b515cbee99ca90b05cd4f97cbb83
Ganesh Khade <ganesh.khade@outlook.com>
2021-04-14 05:35:53 +0530

    Renamed main to app scss

--- stubs/default/resources/views/layouts/guest.blade.php
+++ stubs/default/resources/views/layouts/guest.blade.php
@@ -11,10 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <!--
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        -->
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
         <style type="text/css" media="screen">
             html,
             body {
